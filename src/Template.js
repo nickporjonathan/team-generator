5e1f8d73bb5d567ea5baeb7936f5f96bad6847d8
nickporjonathan <nickporjonathan@gmail.com>
2022-04-05 22:16:14 -0400

    implented HTML code that generates the team cards

--- src/Template.js
+++ src/Template.js
@@ -34,3 +34,34 @@ function GenerateContent(data) {
   });
   return cards;
 }
+
+function GeneratePage(data) {
+  return `
+  <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Staff Profiles</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <header>
+            <h1 class = "head-text">Staff</h1>
+        </header>
+        <section class = "content container text-center">
+            <h2>Our Team</h2><br>
+            <div class = "card-holder row col-8">
+                ${GenerateContent(data)}
+            </div>
+        </section><br>
+        <footer class = "container-fluid text-center">
+            <p class = "col-3 foot1">Profile Generator for Teams</p>
+            <p class = "col -3 foot2">Constructed by Jonathan Nickpor</p>
+        </footer>    
+    </body>
+    </html>`;
+}
