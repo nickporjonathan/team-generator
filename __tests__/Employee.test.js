dd663f1d41c407a8e69545b98ef69f4074d5d7a5
nickporjonathan <nickporjonathan@gmail.com>
2022-04-06 19:22:53 -0400

    employee test page created

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,19 @@
+import Employee from "../lib/Employee";
+
+test("create employee object", () => {
+  const employee = new Employee("testName", 0, "test@email.ca");
+  expect(employee.name).toBe("testName");
+  expect(employee.email).toBe("test@email.ca");
+  expect(employee.id).toEqual(0);
+  expect(employee.role).toBe("Employee");
+  expect(employee.id).toEqual(expect.any(Number));
+});
+
+test("with methods created get employee details", () => {
+  const employee = new Employee("testName", 0, "test@email.ca");
+  expect(employee.getName()).toEqual(expect.stringContaining("testName"));
+  expect(employee.getId()).toEqual(expect.any(Number));
+  expect(employee.getId()).toEqual(0);
+  expect(employee.getEmail()).toEqual(expect.stringContaining("test@email.ca"));
+  expect(employee.getRole()).toBe("Employee");
+});
