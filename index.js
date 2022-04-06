d0457c9f833ab5a80239f79155c7372762269aa5
nickporjonathan <nickporjonathan@gmail.com>
2022-04-06 14:00:44 -0400

    converted const to es6 module

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const Questions = require("./lib/Questions");
-const Template = require("./src/Template");
-const { writeFile, copyFile } = require("./src/Generate");
+import Manager from "./lib/Manager";
+import Engineer from "./lib/Engineer";
+import Intern from "./lib/Intern";
+import { prompt } from "inquirer";
+import { manager } from "./lib/Questions";
+import Template from "./src/Template";
+import { writeFile, copyFile } from "./src/Generate";
 
 let team = [];
 
 function CreateManager() {
-  inquirer.prompt(Questions.manager).then(function newEmployee(data) {
+  prompt(manager).then(function newEmployee(data) {
     let newManager = new Manager(data.name, data.id, data.email, data.office);
     team.push(newManager);
     CreateTeam();
   });
 }
+
