60de77eea41529170b73912a5ea86e5bed891033
nickporjonathan <nickporjonathan@gmail.com>
2022-04-06 14:08:20 -0400

    implemented function that displays other staff options after manager is selected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import Manager from "./lib/Manager";
 import Engineer from "./lib/Engineer";
 import Intern from "./lib/Intern";
-import { prompt } from "inquirer";
+import inquirer, { prompt } from "inquirer";
 import { manager } from "./lib/Questions";
 import Template from "./src/Template";
 import { writeFile, copyFile } from "./src/Generate";
@@ -10,9 +10,26 @@ let team = [];
 
 function CreateManager() {
   prompt(manager).then((data) => {
-          let newManager = new Manager(data.name, data.id, data.email, data.office);
-          team.push(newManager);
-          CreateTeam();
-      });
+    let newManager = new Manager(data.name, data.id, data.email, data.office);
+    team.push(newManager);
+    CreateTeam();
+  });
 }
 
+function CreateTeam() {
+  inquirer.prompt(Questions.team).then((data) => {
+    if (data.addMember) {
+      if (data.role === "Engineer") {
+        CreateEngineer();
+      } else if (data.role === "Intern") {
+        CreateIntern();
+      } else {
+        console.log("error");
+      }
+    } else if (!data.addMember) {
+      CreateFiles();
+    } else {
+      console.log("error");
+    }
+  });
+}
